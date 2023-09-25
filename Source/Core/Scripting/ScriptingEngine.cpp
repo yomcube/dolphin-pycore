a0453de85646948936cd94f6589033c96dee1954
sowens99 <seanowens@comcast.net>
2023-09-24 22:32:59 -0400

    Scripting: Fix memory-related deadlock

--- Source/Core/Scripting/ScriptingEngine.cpp
+++ Source/Core/Scripting/ScriptingEngine.cpp
@@ -6,6 +6,7 @@
 
 #include "Core/API/Events.h"
 #include "Core/API/Gui.h"
+#include "Core/Core.h"
 #include "Common/Logging/Log.h"
 #include "Python/PyScriptingBackend.h"
 
@@ -18,8 +19,16 @@ ScriptingBackend::ScriptingBackend(std::filesystem::path script_filepath)
   // There is only support for python right now.
   // If there was support for multiple backend, a fitting one could be
   // detected based on the script file's extension for example.
-  m_state = new PyScripting::PyScriptingBackend(script_filepath, API::GetEventHub(), API::GetGui(),
-                                                API::GetGCManip(), API::GetWiiButtonsManip(), API::GetWiiIRManip(), API::GetNunchuckButtonsManip());
+
+  // This must be done on the CPU thread to prevent deadlock if CPU is currently running,
+  // as we may attempt to obtain the CPU lock to read memory.
+  Core::RunOnCPUThread(
+      [this, script_filepath]() {
+        m_state = new PyScripting::PyScriptingBackend(
+            script_filepath, API::GetEventHub(), API::GetGui(), API::GetGCManip(),
+            API::GetWiiButtonsManip(), API::GetWiiIRManip(), API::GetNunchuckButtonsManip());
+      },
+      true);
 }
 
 ScriptingBackend::~ScriptingBackend() {
